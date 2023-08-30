e3beea64f8e76a8e49c2ef4aebe8034e9e023653
root <karthikvirat524@gmail.com>
2023-08-30 05:21:24 +0000

    commit message

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,44 @@
+// Write your code here.
+// Write your code here.
+
+import './index.css'
+import {Component} from 'react'
+
+class NavBar extends Component {
+  /* gets top score and current score from EmojiGame page */
+  /* isGameEnd is used decide whether display navbar score card or not  */
+  renderScores = () => {
+    const {currentScore, topScore, isGameEnd} = this.props
+    if (isGameEnd) {
+      return null
+    }
+    return (
+      <div className="Score-container">
+        <p className="score-label">
+          Score : <span className="score-value">{currentScore}</span>
+        </p>
+        <p className="score-label">
+          Total Score : <span className="score-value">{topScore}</span>
+        </p>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="navbar-container">
+        <div className="logo-title-container">
+          <img
+            className="logo-image"
+            src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+            alt="emoji logo"
+          />
+          <h1 className="logo-title">Emoji Game</h1>
+        </div>
+        {this.renderScores()}
+      </div>
+    )
+  }
+}
+
+export default NavBar
